bcc5bab08d1d018758e4e134430ca95a3ebdd271
Adams Castillo <adams.santos.ap@gmail.com>
2022-11-25 15:27:36 -0300

    ok

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 const baseURL = 'https://lojinha-castillo-api.vercel.app'
 
-export const createUser = axios.create({baseURL})
+const acess = "Access-Control-Allow-Origin: *"
+
+export const createUser = axios.create({baseURL, headers:acess})
 
 //buscar user por ID pega o caminho e as configuracoes 
 export const userById=(path, config={ }) => axios.get(`${baseURL}${path}`, config)
